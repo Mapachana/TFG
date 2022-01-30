66ef961ba3da12bd419c33564a4895b3aa402f92
Mapachana <anabuenrua99@gmail.com>
2022-01-30 23:27:32 +0100

    Elimino dependencia innecesaria

--- codigo/pagina_web/paginaSIS.py
+++ codigo/pagina_web/paginaSIS.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 import dash
-import dash_bootstrap_components as dbc
+
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
