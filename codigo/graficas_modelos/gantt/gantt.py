60c5d71d8a4d05664327ce74a0582af2183963fd
Mapachana <anabuenrua99@gmail.com>
2022-05-08 20:59:09 +0200

    Correcciones memoria

--- codigo/graficas_modelos/gantt/gantt.py
+++ codigo/graficas_modelos/gantt/gantt.py
@@ -2,17 +2,17 @@ import plotly.express as px
 import pandas as pd
 
 df = pd.DataFrame([
-    dict(Task="Bibliografía y estado actual", Start='2021-09-20', Finish='2021-10-04', Resource="A"),
-    dict(Task="Especificación de requisitos", Start='2021-10-05', Finish='2021-10-19', Resource="B"),
-    dict(Task="Diseño del sistema", Start='2021-10-20', Finish='2021-11-04', Resource="C"),
-    dict(Task="Marco teórico", Start='2021-11-05', Finish='2022-03-21', Resource="D"),
-    dict(Task="Modelos discretos", Start='2021-11-05', Finish='2022-01-24', Resource="E"),
-    dict(Task="Modelos continuos", Start='2022-01-25', Finish='2022-03-01', Resource="F"),
-    dict(Task="Ajuste de datos", Start='2022-03-02', Finish='2022-05-02', Resource="G"),
-    dict(Task="Documentación", Start='2021-09-20', Finish='2022-05-31', Resource="H")
+    dict(Task="T1", Start='2021-09-20', Finish='2021-10-04', Resource="A"),
+    dict(Task="T2", Start='2021-10-05', Finish='2021-10-19', Resource="B"),
+    dict(Task="T3", Start='2021-10-20', Finish='2021-11-04', Resource="C"),
+    dict(Task="T4", Start='2021-11-05', Finish='2022-03-21', Resource="D"),
+    dict(Task="T5", Start='2021-11-05', Finish='2022-01-24', Resource="E"),
+    dict(Task="T6", Start='2022-01-25', Finish='2022-03-01', Resource="F"),
+    dict(Task="T7", Start='2022-03-02', Finish='2022-05-02', Resource="G"),
+    dict(Task="T8", Start='2021-09-20', Finish='2022-05-31', Resource="H")
 ])
 
 fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource")
 fig.update_yaxes(autorange="reversed")
-fig.update_layout(showlegend=False)
+fig.update_layout(showlegend=False, yaxis = dict( tickfont = dict(size=16)), xaxis = dict( tickfont = dict(size=16)))
 fig.show()
